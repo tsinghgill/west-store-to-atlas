fb32ebe6f7512b36d408a05994dfdf229078699a
Meroxa <tanveet.gill@gmail.com>
2023-06-08 21:25:13 -0400

    test

--- main.py
+++ main.py
@@ -38,11 +38,9 @@ class App:
             
             destination_db = await turbine.resources("mongo-atlas")
 
-            await destination_db.write(transformed, "alldispensedpills", 
-            {
+            await destination_db.write(transformed, "alldispensedpills", {
                 "transforms": "unwrap",
                 "transforms.unwrap.type": "io.debezium.connector.mongodb.transforms.ExtractNewDocumentState"
-            }
-            )
+            })
         except Exception as e:
             print(e, file=sys.stderr)
